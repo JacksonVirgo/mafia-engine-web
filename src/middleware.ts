55bed10f425fd20c5cc65e083f18c01ead3e29a6
Jack Nash <jacknash42@gmail.com>
2023-08-21 12:44:13 +1000

    add auth

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { withClerkMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
+// export default authMiddleware({
+
+// });
+
+export default withClerkMiddleware((_req: NextRequest) => {
+  console.log("middleware running");
+  return NextResponse.next();
+});
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
