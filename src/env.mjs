a7845495bbe1bfa11929462f96544374fac142df
Jack Nash <jacknash42@gmail.com>
2023-08-23 05:15:42 +1000

    fix required environment variables

--- src/env.mjs
+++ src/env.mjs
@@ -9,7 +9,6 @@ export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string().url(),
 		NODE_ENV: z.enum(["development", "test", "production"]),
-		DISCORD_TOKEN: z.string(),
 		CLERK_SECRET_KEY: z.string(),
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
 	},
@@ -30,7 +29,6 @@ export const env = createEnv({
 	runtimeEnv: {
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
-		DISCORD_TOKEN: process.env.DISCORD_TOKEN,
 		CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
