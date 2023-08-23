9c013a89f38dedcee1894cfb083d77666791bd67
Jack Nash <jacknash42@gmail.com>
2023-08-23 11:42:56 +1000

    fix public route middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-	publicRoutes: ["/", "/login", "/contact", "/about"],
+	publicRoutes: ["/", "/login", "/about"],
 	// afterAuth: () => {},
 	// beforeAuth: () => {},
 	// debug: true
